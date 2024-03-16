5ba9277da207989e88ab72d048d4aa795d4b0e1b
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-03-15 21:46:42 -0400

    Updated ActiveLinkCallback & Set Login Security
    We added task-list as an active link that we can access through the function. Also added login security to task-list page.

--- Assets/Scripts/app.js
+++ Assets/Scripts/app.js
@@ -437,6 +437,7 @@
         case "edit": return displayEdit;
         case "login": return displayLogin;
         case "register": return displayRegister;
+        case "task-list": return DisplayTaskList;
         case "404": return display404;
         default:
           console.error("ERROR: callback does not exist: " + activeLink);
@@ -482,6 +483,8 @@
      */
     function DisplayTaskList()
     {
+        authGuard();
+
         let messageArea = $("#messageArea");
         messageArea.hide();
         let taskInput = $("#taskTextInput");
